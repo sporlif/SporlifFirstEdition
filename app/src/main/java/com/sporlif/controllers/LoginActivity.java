9914113c2472db90127aea96c2fe93446395e4fb
Mauricio Sporlif <sporlif@gmail.com>
2017-05-25 11:27:40 -0500

    Login

--- app/src/main/java/com/sporlif/controllers/LoginActivity.java
+++ app/src/main/java/com/sporlif/controllers/LoginActivity.java
@@ -1,15 +1,47 @@
 package com.sporlif.controllers;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.sporlif.FrmMain;
 import com.sporlif.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView actLoginTxtSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        launchWidgets();
+        launchEvents();
+
     }
+
+    public void launchWidgets(){
+
+        actLoginTxtSignIn = (TextView) findViewById(R.id.actLoginTxtSignIn);
+
+    }
+
+    public void launchEvents(){
+
+        actLoginTxtSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, FrmMain.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
+
