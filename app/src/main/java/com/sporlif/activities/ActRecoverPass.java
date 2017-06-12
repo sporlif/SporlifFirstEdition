b9ba16092828fc26197e4ed7b7078c3c33ca0fd1
Mauricio Sporlif <sporlif@gmail.com>
2017-06-11 23:03:44 -0500

    Recuperar password

---
+++ app/src/main/java/com/sporlif/activities/ActRecoverPass.java
@@ -0,0 +1,15 @@
+package com.sporlif.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sporlif.R;
+
+public class ActRecoverPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_recover_pass);
+    }
+}
